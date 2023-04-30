0196c3bdb3b86aab8c4f633f0bf6fae473ae9735
Fredy034 <131563030+Fredy034@users.noreply.github.com>
2023-04-30 16:51:57 -0500

    Add files via upload

---
+++ PawsInn/Clases/ConnectionToSql.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace PawsInn.Clases
+{
+    internal class ConnectionToSql
+    {
+        private readonly string connectionString;
+
+        public ConnectionToSql()
+        {
+            connectionString = @"Data Source=DESKTOP-65QG3G0\SQLEXPRESS; Initial Catalog=PawsInn; Integrated Security=True;Connect Timeout=30";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ PawsInn/Formularios/frmInicio.cs
@@ -0,0 +1,68 @@
+﻿using PawsInn.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PawsInn.Formularios
+{
+    public partial class frmInicio : Form
+    {
+        cConexion cn = new cConexion();
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnMinimize_MouseEnter(object sender, EventArgs e)
+        {
+            //btnMinimize.BackColor = ColorTranslator.FromHtml("#ADD8E6");
+            btnMinimize.BackColor = Color.LightGray;
+        }
+
+        private void btnMinimize_MouseLeave(object sender, EventArgs e)
+        {
+            btnMinimize.BackColor = Color.Transparent;
+        }
+
+        private void btnClose_MouseEnter(object sender, EventArgs e)
+        {
+            btnClose.BackColor = Color.Red;
+        }
+
+        private void btnClose_MouseLeave(object sender, EventArgs e)
+        {
+            btnClose.BackColor = Color.Transparent;
+        }
+
+        private void custom_Button1_Click(object sender, EventArgs e)
+        {
+            frmNewAccount frm = new frmNewAccount();
+            frm.Show();
+            this.Close();
+        }
+
+        private void custom_Button2_Click(object sender, EventArgs e)
+        {
+            frmSignIn frm = new frmSignIn();
+            frm.Show();
+            this.Close();
+        }
+    }
+}
